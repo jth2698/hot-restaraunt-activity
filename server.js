ac23be7831f42a5c61c3c9e236084a2fac790ca2
Botticelli <alizehssn@gmail.com>
2020-09-30 19:48:31 -0500

    commit changes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var path = require("path");
+
 var app = express();
 var PORT = process.env.PORT || 8080
 
@@ -8,16 +10,32 @@ app.use(express.json());
 
 var data = [{
     routeName: "api-data",
-    name: "",
-    table: "05",
-    date: "10/05/2020",
+    name: "somedude",
+    phone: "555-5555",
+    email: "dude@cool.com",
+    id: 05
 }]
 
+// Setting root page to be "index.html" and serving "index.html" if user navigates to root
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get("/api/data", function(req, res) {
+// Serving "reservation.html" if user navigates to ""./reservation.html." This is where our reservation form should be.
+app.get("/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+// Serving "view.html" if user navigates to the "./view.html." This is the page that will show booked tables and the waiting list.
+app.get("/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+
+// This allows the user to see the data currently stored in the api.
+app.get("/api/data", function (req, res) {
     return res.json(data);
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
